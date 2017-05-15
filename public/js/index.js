0118746601f21e04785bd7b71498900926273ef9
Khalil <mohamed.khalil609@gmail.com>
2017-05-15 12:07:54 +0200

    added Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from:'wonka',
-    text:'welcome to my factory'
-  });
 });
 
 socket.on('disconnect', function() {
